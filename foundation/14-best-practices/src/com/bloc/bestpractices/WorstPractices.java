2cfd571d1273e9619a2402f02b6b4fb3fbb6b26a
simonfraboulet <simon_fraboulet@hotmail.com>
2015-03-18 18:47:37 +0100

    Checkpoint 14 assignment completed

--- foundation/14-best-practices/src/com/bloc/bestpractices/WorstPractices.java
+++ foundation/14-best-practices/src/com/bloc/bestpractices/WorstPractices.java
@@ -12,29 +12,32 @@ public class WorstPractices extends Object {
 	/************************************************/
 
 	public static void main(String [] args) {
-	int MAGIC_NUMBER = WorstPractices.animals(false);
-	MAGIC_NUMBER *= 5;
-	if (MAGIC_NUMBER > 18) {
-	while(MAGIC_NUMBER > 0)
-	{
-	MAGIC_NUMBER--;
-	}
-	}
+	int magicNumber *= 5;
+	if (magicNumber > 18) {
+		while (magicNumber > 0) {
+	magicNumber--;
+			}
+		}
 	}
 
-// animals
-// this method takes in a single parameter, yeswellheresthething. Using a very elaborate and complex algorithm, it calculate a magic number
-// yeswellheresthething: a seed which helps generate the magic number
-// returns: a magical number
-private static int animals(boolean yeswellheresthething)
+/animals
+* this method takes in a single parameter, yeswellheresthething. 
+* Using a very elaborate and complex algorithm, 
+* it calculate a magic number
+* yeswellheresthething: a seed which helps generate the magic number
+/ returns: a magical number
+
+private static int animals(boolean yesWellHeresTheThing)
 {
-		/*
-			Start off with one of these
-		*/
-	int aInt = yeswellheresthething ? 34 : 21;
-	float sparklesfairy = .5f;
-	for (int brown = 0; brown < aInt; brown++) { sparklesfairy *= aInt;
-	} return (int) sparklesfairy * aInt;
+		
+			//Start off with one of these
+		
+	int aint = yesWellHeresTheThing ? 34 : 21;
+	float sparklesFairy = .5f;
+	for (int brown = 0; brown < aInt; brown++) { 
+		sparklesFairy *= aInt;
+	} 
+	return (int) sparklesFairy * aInt;
 }
 
 	/************************************************
